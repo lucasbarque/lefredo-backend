b53074893461a7e5a10daf36efd42eec113bd4ec
Lucas Barque <lucasbarquedasilva@hotmail.com>
2024-09-25 16:20:45 -0400

    feat: change permissions on getById dish

--- src/http/rest/controllers/dishes.controller.ts
+++ src/http/rest/controllers/dishes.controller.ts
@@ -13,7 +13,6 @@ import { RestAuthGuard } from 'src/http/auth/guards/rest-jwt-auth.guard';
 import { CreateDishDTO } from '@inputs/create-dish-input';
 
 @Controller('dishes')
-@UseGuards(RestAuthGuard)
 export class DishesController {
   constructor(private dishesService: DishesService) {}
 
@@ -23,16 +22,19 @@ export class DishesController {
   }
 
   @Get()
+  @UseGuards(RestAuthGuard)
   list(@Query('sectionId') sectionId: string) {
     return this.dishesService.getDishesBySectionId(sectionId);
   }
 
   @Post()
+  @UseGuards(RestAuthGuard)
   create(@Body() data: CreateDishDTO) {
     return this.dishesService.create(data);
   }
 
   @Delete(':id')
+  @UseGuards(RestAuthGuard)
   delete(@Param('id') id: string) {
     return this.dishesService.delete(id);
   }
